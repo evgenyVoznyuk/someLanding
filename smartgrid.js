e0f78973a4db105bc36bf61407f19b091a1d78b5
evgenyVoznyuk <evgeny.voznyuk@gmail.com>
2020-02-11 23:41:24 +0300

    start

---
+++ smartgrid.js
@@ -0,0 +1,24 @@
+module.exports = {
+  columns: 12,
+  offset: "30px",
+  container: {
+    maxWidth: "1180px",
+    fields: "15px"
+  },
+  breakPoints: {
+    lg: {
+      width: "1200px"
+    },
+    md: {
+      width: "992px",
+      fields: "15px"
+    },
+    sm: {
+      width: "720px"
+    },
+    xs: {
+      width: "576px"
+    }
+  },
+  mobileFirst: false
+}
